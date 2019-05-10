22eaed1cfa0c1ddf3f3b8fb0a4a058dbf98c9774
MuratovAS <lexamzx@gmail.com>
2019-05-10 20:05:09 +0500

    ed

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ struct EEpromStruct {
 
 /////////////////////////////////////////////////////////////////////////////////display
 //128x64
+
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 16, /* data=*/ 17);
 //U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 16, /* data=*/ 17);
 
